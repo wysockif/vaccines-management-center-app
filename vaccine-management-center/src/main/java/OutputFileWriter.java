d263fd4b90c349e1b26d234eaea93f8052facc00
Franek <wysocki.f.00@gmail.com>
2020-11-15 14:37:34 +0100

    Added an output file support & code refactoring

---
+++ vaccine-management-center/src/main/java/OutputFileWriter.java
@@ -0,0 +1,55 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class OutputFileWriter {
+    public static String fileName;
+    public int longestLength;
+
+    public OutputFileWriter(String path){
+        File file = new File(path);
+        fileName = file.getName();
+        longestLength = findLongestLength();
+        PrintWriter printWriter = createPrintWriterIfFileExists(file);
+        String line = concatenateLine(longestLength, "Eko Polska 2020", "CentMedEko 24h", 100, 50.5 );
+        saveLineToFile(printWriter, line);
+
+        line = concatenateLine(longestLength, "Post-Covid Sp. z o.o.", "CentMedEko Nowogrodzka", 500, 10.5 );
+        saveLineToFile(printWriter, line);
+
+
+        printWriter.close();
+    }
+
+    private int findLongestLength() {
+        return 25;
+    }
+
+    public void saveLineToFile(PrintWriter printWriter, String line){
+        printWriter.println(line);
+    }
+
+    public String concatenateLine(int length, String producerName, String pharmacyName, int amount, double price){
+        producerName = rightPad(producerName, length);
+        String line = producerName + " -> " + pharmacyName +
+                " [Koszt = " + amount + " * " + price + " = " + amount*price + " zł]";
+        return line;
+    }
+
+    private String rightPad(String text, int length) {
+        return String.format("%-" + length + "." + length + "s", text);
+    }
+
+    private PrintWriter createPrintWriterIfFileExists(File outputFile) {
+        PrintWriter printWriter = null;
+        try{
+            printWriter = new PrintWriter(outputFile);
+        }catch (FileNotFoundException e){
+            String message = "[Plik wyjściowy: " + fileName + "]. Plik nie został znaleziony.";
+                    Errors.handleTheError(Errors.OUTPUT_FILE_NOT_FOUNT, message);
+        }
+        return printWriter;
+    }
+
+
+}
--- vaccine-management-center/src/main/java/items/Producers.java
+++ vaccine-management-center/src/main/java/items/Producers.java
@@ -1,27 +1,20 @@
 package items;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.jetbrains.annotations.NotNull;
 
-public class Producers {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.zip.DataFormatException;
+
+public class Producers implements Item, Iterable<Producer> {
     private final List<Producer> producers;
 
     public Producers() {
         producers = new ArrayList<>();
     }
 
-    public void addANewProducer(Producer producer) {
-        if (producers.contains(producer)) {
-            throw new UnsupportedOperationException("Nie można dodawać producentów o tym samym id!");
-        }
-
-        if(producer.getDailyProduction() < 0 ){
-            throw new IllegalArgumentException("Dziena produkcja nie może być ujemna!");
-        }
-        producers.add(producer);
-    }
-
-    public boolean contain(long id) {
+    public boolean alreadyContains(long id) {
         for (Producer p : producers) {
             if (p.getId() == id) {
                 return true;
@@ -30,15 +23,54 @@ public class Producers {
         return false;
     }
 
-    public int getNumberOfProducers(){
+    public int getNumberOfProducers() {
         return producers.size();
     }
 
-    public Producer getProducer(long id){
-        for(Producer p : producers){
-            if(p.getId() == id)
+    public Producer getProducer(long id) {
+        for (Producer p : producers) {
+            if (p.getId() == id)
                 return p;
         }
         return null;
+    }
+
+
+    @Override
+    public Object[] convertAttributes(String[] attributes) throws DataFormatException {
+        if (attributes.length != 3) {
+            throw new DataFormatException("Niepoprawny format danych");
+        }
+        return parseAttributes(attributes);
+    }
+
+    @Override
+    public void validateAttributes(Object[] attributes) throws DataFormatException {
+        int dailyProduction = (int) attributes[2];
+        long id = (long) attributes[0];
+        if (dailyProduction < 0) {
+            String message = "Niepoprawny format danych. Ujemna wartość reprezentująca dzienną produkcję";
+            throw new DataFormatException(message);
+        }
+        if (alreadyContains(id)) {
+            String message = "Nie można dodawać producentów o tym samym id";
+            throw new DataFormatException(message);
+        }
+    }
+
+    @Override
+    public void addNewElement(Object[] attributes) {
+        long id = (long) attributes[0];
+        String name = (String) attributes[1];
+        int dailyProduction = (int) attributes[2];
+        Producer producer = new Producer(id, name, dailyProduction);
+        producers.add(producer);
+        System.out.println(producer);
+    }
+
+    @NotNull
+    @Override
+    public Iterator<Producer> iterator() {
+        return producers.iterator();
     }
 }
